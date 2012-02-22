56a57b0888f1be830f98c7f59964778917e87ddc
John Griffin <johnog@gmail.com>
2012-02-22 21:05:55 +0000

    switch to zepto

--- app.coffee
+++ app.coffee
@@ -1,4 +1,4 @@
-$ = jQuery
+$ = Zepto
 $ -> 
   game = new Game
   game.run()
--- app.js
+++ app.js
@@ -4,7 +4,7 @@
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; },
     __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
-  $ = jQuery;
+  $ = Zepto;
 
   $(function() {
     var game;
